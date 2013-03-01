ffb2daa316b487d72b68720c7ec214321ed320ac
Jim <jim432115@gmail.com>
2013-02-28 16:01:25 -0800

    First Deaft

---
+++ ContactArray.java
@@ -0,0 +1,42 @@
+/**
+ * this class contains all the methods for editing the 
+ * array of contacts.
+ * @author Jim
+ *
+ */
+public class ContactArray {
+  /**
+	 * add new people
+	 */
+	public void newPeople(){
+		System.out.println("Adding People.\n\n\n\n");
+	}
+	/**
+	 * print out all contacts 
+	 */
+	public String toString(){
+		return "Printing all contacts.\n\n\n\n";
+	}
+	/**
+	 * search contacts by last name
+	 * @return
+	 */
+	public String searchByName(){
+		return "Searching by last name.\n\n\n\n";
+	}
+	/**
+	 * search contacts by email address
+	 * @return
+	 */
+	public String searchByEmail(){
+		return "Searching by Email.\n\n\n\n";
+	}
+	/**
+	 * display all contacts who have the same zip code
+	 * @return
+	 */
+	public String searchByZip(){
+		return "Displaying everyone living in this ZIP.\n\n\n\n";
+	}
+
+}
